79fb7369ee7a6ccc0493e70d8fec1ac3cc39548a
Facio <66415135+maciej-debowski@users.noreply.github.com>
2022-03-24 11:12:48 +0100

    Update index.ts
    adding more imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+/**
+  * Exporting all essentials and utilities for PinaWeb
+  */
+
 export { App } from "./main/App"
 export { Page } from "./main/Page"
+export { Module, ModuleRenderer } from "./classess/Module.ts"
+export { Component  } from "./classess/Component.ts"
+export { compile } from "./compile/index"
+export { PinaModule } from "./config/index"
+export { PinaStyles } from "./config/index"
+export { PinaWebsiteConfig } from "./config/index"
